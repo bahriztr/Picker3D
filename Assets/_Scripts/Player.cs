bace579621e7d0d80ac616c1ec788a9afac6cb14
bahriztr <bahriztr3432@gmail.com>
2022-09-25 18:06:41 +0300

    Scene Update

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private FloatingJoystick joystick;
-    byte playerSpeed = 4;
+    [SerializeField] byte playerSpeed = 8;
+    [SerializeField] byte swipeSpeed = 3;
     float xDir;
 
-    private void Update()
+    private void FixedUpdate()
     {
         xDir = joystick.Horizontal;
         transform.Translate(new Vector3(xDir * 3,0,1) * playerSpeed * Time.deltaTime);
