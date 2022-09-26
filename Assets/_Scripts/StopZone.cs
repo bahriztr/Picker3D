4b5220ef41f1cf5d7fe6b15c93f57c0b9f07ea84
bahriztr <bahriztr3432@gmail.com>
2022-09-26 16:58:43 +0300

    Scene Update

--- Assets/_Scripts/StopZone.cs
+++ Assets/_Scripts/StopZone.cs
@@ -6,6 +6,7 @@ public class StopZone : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        //magnet topladýðýmýz eþyalarý kovaya ittirmeli
         if (other.CompareTag("Player"))
             Player.Instance.PlayerSpeed = 0f;
     }
